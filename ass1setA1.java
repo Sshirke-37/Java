cba119cf3ce71c71245d7f1d9d77e6ffef876fd8
Soham <150108922+Sshirke-37@users.noreply.github.com>
2025-01-05 11:20:21 +0530

    Update ass1setA1.java

--- ass1setA1.java
+++ ass1setA1.java
@@ -1,3 +1,6 @@
+Accept ‘n’ integers from the user and store them in a collection. Display them in the sorted order. The collection should not accept duplicate elements. (Use a suitable 
+collection). Search for an particular element using predefined search method in the Collection framework.
+ 
 import java.io.*;
 import java.util.*;
 
