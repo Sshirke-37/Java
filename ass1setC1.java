25820116049b6d467c61b13e49f30f547b878e66
Soham <150108922+Sshirke-37@users.noreply.github.com>
2025-01-05 11:22:54 +0530

    Update ass1setC1.java

--- ass1setC1.java
+++ ass1setC1.java
@@ -1,3 +1,10 @@
+Read a text file, specified by the first command line argument, into a list. The program should then display a menu which performs the following operations on the list: 
+1. Insert line 
+2. Delete line
+3. Append line 
+4. Modify line 5. Exit 
+When the user selects Exit, save the contents of the list to the file and end the program. 
+
 import java.io.*;
 import java.util.*;
 
