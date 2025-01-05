2d39893b032f09bb2cf11a50bb1f027ac1620b3a
Soham <150108922+Sshirke-37@users.noreply.github.com>
2025-01-05 11:21:17 +0530

    Update ass1setA2.java

--- ass1setA2.java
+++ ass1setA2.java
@@ -1,5 +1,9 @@
-import java.util.*;
+Construct a linked List containing names of colors: red, blue, yellow and orange. Then extend your program to do the following:  
+i.Display the contents of the List using an Iterator;  
+ii. Display the contents of the List in reverse order using a ListIterator;  
+iii. Create another list containing  pink and  green. Insert the elements of this list between blue and yellow.  
 
+import java.util.*;
 public class ass1setA2
 {
 	public static void main(String args[])
