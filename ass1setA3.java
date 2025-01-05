7de333def81eee59a22a60e9c64fd7d536913b36
Soham <150108922+Sshirke-37@users.noreply.github.com>
2025-01-05 11:21:44 +0530

    Update ass1setA3.java

--- ass1setA3.java
+++ ass1setA3.java
@@ -1,3 +1,5 @@
+Create a Hash table containing student name and percentage. Display the details of the hash table. Also search for a specific student and display percentage of that student. 
+
 import java.util.*;
 import java.io.*;
 import java.util.Map.Entry;
