032252d2b333b892f16efbbf7119616fcd895d72
Soham <150108922+Sshirke-37@users.noreply.github.com>
2025-01-05 11:17:42 +0530

    Add files via upload

---
+++ ass1setA1.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class ass1setA1
+{
+	public static void main(String args[]) throws Exception
+	{
+		int s=1,no,element,i;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		TreeSet ts=new TreeSet();
+		System.out.println("Enter how many numbers you want:");
+		no=Integer.parseInt(br.readLine());
+		System.out.println("Enter "+no+" numbers:");
+		for(i=0;i<no;i++)
+		{
+			element=Integer.parseInt(br.readLine());
+			ts.add(element);
+		}
+		System.out.println("The elements in the sorted order:"+ts);
+
+
+		while(s==1)
+		{
+			System.out.println("Enter the element to search:");
+			element=Integer.parseInt(br.readLine());
+			if(ts.contains(element))
+				System.out.println("Element is found!");
+			else
+				System.out.println("Element not found!");
+			System.out.println("Search Again?");
+			s=Integer.parseInt(br.readLine());
+		}
+	}
+
+}
---
+++ ass1setA2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ass1setA2
+{
+	public static void main(String args[])
+	{
+		LinkedList t=new LinkedList();
+		t.add("Red");
+		t.add("Blue");
+		t.add("Yellow");
+		t.add("Orange");
+		Iterator i=t.iterator();
+		System.out.println("Elements:");
+		while(i.hasNext())
+			System.out.println(i.next());
+		ListIterator i1=t.listIterator();
+		System.out.println("\n");
+		while(i1.hasNext())
+			i1.next();
+		System.out.println("Elements in reversed order:");
+		while(i1.hasPrevious())
+			System.out.println(i1.previous());
+		System.out.println("\n");
+		LinkedList t1=new LinkedList();
+		t1.add("Pink");
+		t1.add("Green");
+		i=t1.iterator();
+		t.add(2,i.next());
+		t.add(3,i.next());
+		Iterator i2=t.iterator();
+		System.out.println("Total Elements:");
+		while(i2.hasNext())
+			System.out.println(i2.next());
+	}
+}
+
